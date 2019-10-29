0ab61486235ba765591fff9bba4cd432e2159ba8
Lihanzhe <352974151@qq.com>
2019-10-29 20:26:47 +0800

    soft-test-mylang

---
+++ src/main/Mylang.java
@@ -0,0 +1,77 @@
+package main;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
+public class Mylang {
+	public boolean Judge(String buff) {		
+		return StringUtils.isBlank(buff);
+	}
+	public void solution(String buff) {
+		if(StringUtils.isBlank(buff)!=true)
+		{
+			
+		}
+		StringUtils.indexOf(buff,"a");
+	
+	}
+	
+	public String Connect(String[] str,int n) {
+		
+		for(int i=1;i<=n;i++)
+		{
+			if(StringUtils.isBlank(str[i]))
+				return null;
+		}
+		String temp;
+		for(int i=1;i<=n;i++)
+		{
+			str[i]=StringUtils.capitalize(str[i]);
+			System.out.println(str[i]);
+		}
+
+		temp=StringUtils.getCommonPrefix(str[1],str[2],str[3]);
+		System.out.println(temp);
+		if(temp=="")
+		{
+			System.out.println("The Prefix is NULL");
+		}
+		else if(temp.length()>3)//长度大于3
+		{
+			for(int i=1;i<=n-1;i++)
+			{
+				temp+=StringUtils.join(str[i],str[1+i]);
+			}
+		}
+		else//长度大于等于0，小于等于3
+		{
+			for(int i=1;i<=n;i++)
+			{
+				str[i]=StringUtils.upperCase(str[i]);
+			}
+			for(int i=1;i<=n-1;i++)
+			{
+				temp+=StringUtils.join(str[i],str[1+i]);
+			}
+		}
+		return temp;
+	}
+	
+	public String[] Create(int n) {
+		String []str=new String [n+1];
+		String chars="aaaaaab";
+		str[0]="";
+		for(int i=1;i<=n;i++)
+		{
+			str[i]=RandomStringUtils.random(n,chars);
+			System.out.println(str[i]);
+		}
+		return str;
+	}
+	public static void main(String args[]) {
+		int n=5;
+		Mylang ml=new Mylang();
+		String []str=ml.Create(5);		
+		System.out.println(ml.Connect(str,n));
+	}
+}
---
+++ test/main/MylangTest.java
@@ -0,0 +1,40 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class MylangTest {
+	private Mylang ml;
+	@BeforeEach
+	void setUp() throws Exception {
+		ml=new Mylang();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testJudge() {
+		String buff="abcd";
+		assertEquals(false, ml.Judge(buff));
+	}
+	
+	@Test
+	void testConnect() {
+		String []str=new String[6];
+		int n=5;
+		str[0]="";
+		for(int i=1;i<=n;i++)
+		{
+			str[i]="ab";
+		}
+		System.out.println(ml.Connect(str,n));
+		assertEquals("AbAbAbAbAbAbAbAb", ml.Connect(str,5));
+	}
+
+}
